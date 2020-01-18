ae867d2da9ced0886035367dee6ad81c3cc7db64
Matthew Chan <matthewchan@mc26-noyes.scs.illinois.edu>
2020-01-17 18:11:51 -0600

    CXCR4-CCR5 case study

--- transfer_mut.py
+++ transfer_mut.py
@@ -1,44 +1,71 @@
-from jmhe import jmhe as jmhe
+from jmhe import jmhe 
 import pandas as pd
 from jmhe import score
 import pickle
 
-#Define couplings model of source protein, training and testing deep mutatgenesis datasets
-Couplings_model = 'ubiquitin.model'
-train_Ad = "trainingdataset.csv"
-test_Ad = "testdataset.csv"
+Couplings_model_source = '/home/zshamsi2/projects/functionalCouplings/CXCR4-CCR5/jmhe/CXCR4/Model.model'
+dataAdress_source = "/home/zshamsi2/projects/functionalCouplings/CXCR4-CCR5/jmhe/CXCR4/BiFC/exp.csv"
 
-tr = pd.read_csv(train_Ad, sep=",", comment="#")
-te = pd.read_csv(test_Ad, sep=",", comment="#")
+# Train the source model based on its own DMS experiments
+data = pd.read_csv(dataAdress_source, sep=",", comment="#")
+model_source = jmhe(Couplings_model=Couplings_model_source)
 
-model = jmhe(Couplings_model=Couplings_model)
+# Predict from initial model
+dx2 = model_source.predict(X=dataAdress)
+s1 = score(dx2, data_exp=dataAdress)
 
-# train the source protein based on DMS experiments
-train_predict_source = model.predict(X=train_Ad)
-train_score_source = score(train_predict_source, data_exp=train_Ad)
-test_predict_source = model.predict(X=test_Ad)
-test_score_source = score(test_predict_source, data_exp=test_Ad)
+print('Score for CXCR4 before training: ', s1)
 
-print('Score before training on the training dataset: ', train_score_source)
-print('Score before training on the test dataset: ', test_score_source) 
+# Predict after fitting with experimental data
 
-
-model.sp = train_score_source[0]
-if train_score_source[1]>0:
-  model.positiveL = True
+model_source.sp = s1[0]
+if s1[1]>0:
+  model_source.positiveL = True
 else:
-  model.positiveL = False
+  model_source.positiveL = False
+print(model_source.positiveL)
+model_source.fit(data)
+pickle.dump(model_source, open('model_cxcr4.pkl', 'wb'))
+#model_source = pickle.load(open('model_cxcr4.pkl', 'rb'))
 
-print(model.positiveL)
-model.fit(tr)
-pickle.dump(model, open('model.pkl', 'wb'))
+dx = model_source.predict(X=dataAdress)
 
-# Predict the traget protein after fitting with training and testing experimental data
-train_predict_target = model.predict(X=train_Ad)
-train_score_target = score(train_predict_target, data_exp=train_Ad)
+# Calculate the Spearman's correlation score
+s2 = score(dx, data_exp=dataAdress)
+print('Score for CXCR4 after training: ', s2)
 
-test_predict_target = model.predict(X=test_Ad)
-test_score_target = score(test_predict_target, data_exp=test_Ad)
 
-print('Score after training on the training dataset: ', train_score_target)
-print('Score after training on the test dataset: ', test_score_target) 
+# For comparison, predict the target model without transfer
+
+# Model of target protein
+Couplings_model_target='/home/zshamsi2/projects/functionalCouplings/CXCR4-CCR5/jmhe/CCR5/Model.model'
+
+# Path of experimental DMS dataset
+dataAdress_target = "/home/zshamsi2/projects/functionalCouplings/CXCR4-CCR5/jmhe/CCR5/BiFC/exp.csv"
+data_target_target = pd.read_csv(dataAdress_target, sep=",", comment="#")
+
+model_target = jmhe(Couplings_model=Couplings_model_target)
+
+dx2 = model_target.predict(X=dataAdress_target)
+
+# Calculate the Spearman's correlation score
+s1 = score(dx2, data_exp=dataAdress_target)
+print('Score for CCR5 before transfer: ',s1)
+
+
+# Transfer from source protein to target protein 
+
+# Sequence alignment file can be generated from any alignment server or program 
+algn_file = '/home/zshamsi2/projects/functionalCouplings/CXCR4-CCR5/case_PMEP_jmhe/CXCR4-CCR5.aln'
+
+model_target_transfer = model_source.transfer(Couplings_model_target=Couplings_model_target, algn_file=algn_file)
+#pickle.dump(model_target_transfer, open('model_ccr5_transfered.pkl', 'wb'))
+
+dx2_transfer = model_target_transfer.predict(X=dataAdress_target)
+
+# Calculate the Spearman's correlation score
+s1_transfer = score(dx2_transfer, data_exp=dataAdress_target)
+print('Score for CCR5 after transfer from CCR5', s1_transfer)
+
+
+
