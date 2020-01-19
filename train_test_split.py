e00fe81c4aa3ae075f7b627fe4c81d23d835ab22
Matthew Chan <matthewchan@mc26-noyes.scs.illinois.edu>
2020-01-19 12:05:45 -0600

    Added comments

--- train_test_split.py
+++ train_test_split.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import numpy as np
 
-k = 1
-dataAddress= "ubiquitin_exp.csv"
+#Number of folds
+k = 5
+
+dataAddress= "CXCR4_exp.csv"
+
 df = pd.read_csv(dataAddress, sep=",", comment="#")
 
 for f in range(k):
